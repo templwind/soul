062c1043595d79dc28c3516d16ed140d2a349ab4
Alma Tuck <alma.tuck@gmail.com>
2024-10-07 00:58:46 -0600

    updated pubsub and added redis for auto id management

--- pubsub/pubsub.go
+++ pubsub/pubsub.go
@@ -4,7 +4,7 @@ import (
 	"time"
 )
 
-type MessageBroker interface {
+type Broker interface {
 	Publish(subject string, message []byte, msgID ...string) error
 	Subscribe(subject string, handler func([]byte) ([]byte, error)) error
 	CreateStream(streamName, subject string, dedupWindow time.Duration) error
