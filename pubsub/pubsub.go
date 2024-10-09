21e7f7fe66fae8791bf7a59e9d0a16539c3072a9
Alma Tuck <alma.tuck@gmail.com>
2024-10-09 00:11:07 -0600

    updating comments

--- pubsub/pubsub.go
+++ pubsub/pubsub.go
@@ -6,12 +6,14 @@ import (
 	"time"
 )
 
+// Broker is the interface that wraps the methods for a message broker
 type Broker interface {
 	Publish(subject string, message []byte, msgID ...string) error
 	Subscribe(subject string, group string, handler func([]byte) ([]byte, error)) error
 	CreateStream(streamName, subject string, dedupWindow time.Duration) error
 }
 
+// Marshal marshals the given value to a JSON byte slice
 func Marshal(v any) []byte {
 	b, err := json.Marshal(v)
 	if err != nil {
